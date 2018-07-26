4ae4822071490c9c7ce1540aa92667a5698c8f6d
Ronald Tong <tongrd@jesus-2.local>
2018-07-26 11:16:38 +1000

    Initial Commit

---
+++ Todoeyy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Todoeyy
+//
+//  Created by Ronald Tong on 26/7/18.
+//  Copyright © 2018 StokeDesign. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
