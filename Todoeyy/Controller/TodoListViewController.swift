eefdf99e18a9326e6a4c5371407e0414e022f078
Ronald Tong <tongrd@192-168-1-100.tpgi.com.au>
2018-07-29 12:00:28 +1000

    Implement Core Data and Search Bar functionality.

--- Todoeyy/Controller/TodoListViewController.swift
+++ Todoeyy/Controller/TodoListViewController.swift
@@ -7,20 +7,26 @@
 //
 
 import UIKit
+import CoreData
 
 class TodoListViewController: UITableViewController {
 
     var itemArray = [Item]()
-    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    
+    // Locate the UI Application class, find the shared singleton object, locate it's delegate (App Delegate) and cast that as App Delegate object
+    // Then find the persistentContainer and viewContext properties
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+                
+        print("*** Document Directory \(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))")
+
         loadItems()
         
     }
 
-   //MARK - Tableview Datasource Methods
+    //MARK: - Tableview Datasource Methods
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
@@ -42,24 +48,19 @@ class TodoListViewController: UITableViewController {
         
     }
     
-    //MARK - TableView Delegate Methods
+    //MARK: - TableView Delegate Methods
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        toggleCheckmark(indexPath: indexPath)
+        // Toggle checked attribute 
+        itemArray[indexPath.row].checked = !itemArray[indexPath.row].checked
+        
         saveData()
         tableView.deselectRow(at: indexPath, animated: true)
-        
+   
     }
     
-    func toggleCheckmark(indexPath: IndexPath) {
-        
-        let selectedItem = itemArray[indexPath.row]
-        selectedItem.checked = !selectedItem.checked
-       
-    }
-    
-    //MARK - Add New Items
+    //MARK: - Add New Items
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
@@ -70,8 +71,9 @@ class TodoListViewController: UITableViewController {
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             //What happens if user clicks Add Item Button on UI Alert
             
-            let newItem = Item()
+            let newItem = Item(context: self.context)
             newItem.title = newItemText.text!
+            newItem.checked = false
             self.itemArray.append(newItem)
             
             self.saveData()
@@ -89,24 +91,52 @@ class TodoListViewController: UITableViewController {
     }
     
     func saveData() {
-        let encoder = PropertyListEncoder()
         
         do {
-            let data = try encoder.encode(itemArray)
-            try data.write(to: dataFilePath!)
+            try context.save()
         } catch {
-            print("Error encoding item array, \(error)")
+            print("***Error saving context \(error)")
         }
         tableView.reloadData()
     }
     
-    func loadItems() {
-        if let data = try? Data(contentsOf: dataFilePath!) {
-         let decoder = PropertyListDecoder()
-            do { itemArray = try decoder.decode([Item].self, from: data)
+    // LoadItems called with an argument will use NSFetchRequest<Item>
+    // If LoadItems is called with no argument it will use the default value Item.fetchRequest()
+    
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest()) {
+        do {
+            itemArray = try context.fetch(request)
         } catch {
-            print("Error decoding item array, \(error)")
+            print("***Error fetching data from context \(error)")
         }
+        tableView.reloadData()
     }
 }
+
+//MARK: - Search Bar Methods
+
+extension TodoListViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+
+        // For each item in the array, look for the item with title containing %@ searchBar.text. Modify the request with predicate and then sort.
+        let searchRequest : NSFetchRequest<Item> = Item.fetchRequest()
+        
+        searchRequest.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        
+        searchRequest.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        
+        loadItems(with: searchRequest)
+        
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            loadItems()
+            
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+        }
+    }
 }
